9e107b698fca4087f24adf2b68b0c479beea22a7
Никита Федоров <nikfedorov65@gmail.com>
2021-03-22 21:27:55 +0600

    Добавьте файлы проекта.

---
+++ WoT/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Random rnd = new Random();
+                Tank[] winner = new Tank[5];
+                Tank[] T34 = new Tank[5];
+                Tank[] Panther = new Tank[5];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    T34[i] = new Tank("T34", rnd.Next(0, 100), rnd.Next(0, 100), rnd.Next(0, 100));
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Panther[i] = new Tank("Panther", rnd.Next(0, 100), rnd.Next(0, 100), rnd.Next(0, 100));
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    winner[i] = T34[i] * Panther[i];
+                    Console.WriteLine(winner[i].ShowInfo());
+                }
+            }
+
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            
+        }
+    }
+}
---
+++ WoT/Tank.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoT
+{
+    class Tank
+    {
+        public string Name { get; set; }
+        public int AmmunitonLvl { get; set; }
+        public int ArmorLvl { get; set; }
+        public int ManeuverabilityLvl { get; set; }
+
+        public Tank(string Name,int AmmunitonLvl, int ArmorLvl, int ManeuverabilityLvl)
+        {
+            //Random rnd = new Random();
+            //AmmunitonLvl = rnd.Next(1, 100);
+            //ArmorLvl = rnd.Next(1, 100);
+            //ManeuverabilityLvl = rnd.Next(1, 100);
+
+            this.Name = Name;
+            this.AmmunitonLvl = AmmunitonLvl;
+            this.ArmorLvl = ArmorLvl;
+            this.ManeuverabilityLvl = ManeuverabilityLvl;
+        }
+
+        public Tank()
+        {
+            Name = "null";
+            AmmunitonLvl = 0;
+            ArmorLvl = 0;
+            ManeuverabilityLvl = 0;
+        }
+
+        public static Tank operator *(Tank T34, Tank Panther)
+        {
+            if (T34.AmmunitonLvl > Panther.AmmunitonLvl && T34.ArmorLvl > Panther.ArmorLvl)
+            {
+                return T34;
+            }
+            if (T34.AmmunitonLvl > Panther.AmmunitonLvl && T34.ManeuverabilityLvl > Panther.ManeuverabilityLvl)
+            {
+                return T34;
+            }
+            if (T34.ArmorLvl > Panther.ArmorLvl && T34.ManeuverabilityLvl > Panther.ManeuverabilityLvl)
+            {
+                return T34;
+            }
+            else
+            {
+                return Panther;
+            }
+        }
+
+        public string ShowInfo()
+        {
+            int sum = AmmunitonLvl + ArmorLvl + ManeuverabilityLvl;
+            Console.WriteLine("Победивший танк: " + Name);
+            Console.WriteLine("Размер боекомплекта: " + AmmunitonLvl);
+            Console.WriteLine("Уровень брони: " + ArmorLvl);
+            Console.WriteLine("Уровень маневренности: " + ManeuverabilityLvl);
+            Console.WriteLine("Суммарный уровень: " + sum);
+            Console.WriteLine("-----------------------------------");
+
+            return "";
+        }
+
+
+    }
+}
